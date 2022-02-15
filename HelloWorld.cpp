2cd537cdc870d6f4f94c95285883dc79aedd02ab
Mario Knaus <mario@student-net-hci-2566.intern.ethz.ch>
2022-02-15 23:07:38 +0100

    third update

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,8 +2,9 @@
 
 int main() {
     std::cout << "\nHello World\n";
-    for (unsigned int i {0}; i < 13; ++i) {
+        for (unsigned int i {0}; i < 13; ++i) {
         std::cout << i << '\n';
     }
+    std::cout<<"\nWhat's wrong with you?\n";
     return 0;
 }
