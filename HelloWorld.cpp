6208198a80d80075c13785ffe5ef6539079e1ed2
Mario Knaus <mario@Marios-MacBook-Air.local>
2022-02-27 13:29:02 +0100

    fixed for-loop

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     std::cout << "\nHello World\n";
-        for (unsigned int i {0}; i < 13; ++i) {
+    for (unsigned int i {0}; i < 13; ++i) {
         std::cout << i << '\n';
     }
     std::cout<<"\nWhat's wrong with you?\n";
--- README.md
+++
@@ -1,7 +0,0 @@
-# This is just for testing purposes
-
-I'm still learning how to handle GitHub so don't laugh at my repositories okay?
-
-## New Topic
-
-Does anyone know a good cookie recipe?
